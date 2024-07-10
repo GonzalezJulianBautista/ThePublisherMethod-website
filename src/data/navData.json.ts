b272b79f0153856a1975c4681495bd635cb5ee24
Julián Bautista González <gonzalezjulianbautista@gmail.com>
2024-07-10 20:49:15 -0300

    First commit to repo
    Siguiendo un tutorial de youtube tiré todos los archivos en un repo

---
+++ src/data/navData.json.ts
@@ -0,0 +1,18 @@
+export const navLinks = [  
+    {
+        text: "Home",
+        href: "/",
+    },
+    {
+        text: "Services",
+        href: "/services/",
+    },
+    {
+        text: "About The Method",
+        href: "/about/",
+    },
+    {
+        text: "Portfolio",
+        href: "/portfolio/",
+    },
+]
---
+++ tina/config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch: "main",
+
+  // Get this from tina.io
+  clientId: "305e3296-2ebf-43cd-9dd8-e8979e6d776b",
+  // Get this from tina.io
+  token: "40cf4278f05e4379a081223e997e2e0acac5ed12",
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
